baac7c663800a19feed72925e457be2fbe5e1d22
lns13301 <rkdrbgks22@ks.ac.kr>
2020-10-20 16:31:55 +0900

    feat: Add distribute cards.
    - 딜러와 유저에게 카드를 분배
    - 분배 메세지 출력

--- src/main/java/domain/game/GameManager.java
+++ src/main/java/domain/game/GameManager.java
@@ -2,6 +2,7 @@ package domain.game;
 
 import domain.card.Card;
 import domain.card.CardFactory;
+import domain.user.Dealer;
 import view.Output;
 
 import java.util.List;
@@ -9,10 +10,24 @@ import java.util.List;
 public class GameManager {
     private List<Card> deck = CardFactory.create();
     private int pickUpIndex = 0;
+    private Dealer dealer = new Dealer();
     private Participant participant;
 
     public void startGame() {
         String[] names = Output.showWhoJoinGame();
         participant = new Participant(Output.showHowMuchBetting(names));
+        distributeCard();
+    }
+
+    public void distributeCard() {
+        Output.showGameProgressing(participant.getPlayers());
+
+        dealer.addCard(deck.get(pickUpIndex));
+        pickUpIndex++;
+
+        pickUpIndex = participant.pickCardFromDeck(deck, pickUpIndex);
+
+        Output.showCardStateDealer(dealer);
+        Output.showCardStatePlayer(participant.getPlayers());
     }
 }
--- src/main/java/domain/game/Participant.java
+++ src/main/java/domain/game/Participant.java
@@ -1,14 +1,36 @@
 package domain.game;
 
+import domain.card.Card;
 import domain.user.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Participant {
-    List<Player> players;
+    private List<Player> players;
 
     public Participant(List<Player> players) {
         this.players = players;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public int pickCardFromDeck(List<Card> cards, Player player, int index) {
+        player.addCard(cards.get(index));
+        return ++index;
+    }
+
+    // 처음 2장씩 분배
+    public int pickCardFromDeck(List<Card> cards, int index) {
+        for (Player player : players) {
+            player.addCard(cards.get(index));
+            index++;
+            player.addCard(cards.get(index));
+            index++;
+        }
+
+        return index;
     }
 }
